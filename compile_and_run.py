23ec6e16c968487b3d91349a9e52b1be7caec17f
thatcooldevguy <tsommers150@gmail.com>
2022-02-22 17:53:28 -0500

    git -ff

--- compile_and_run.py
+++ compile_and_run.py
@@ -6,7 +6,7 @@ import os
 print("** Compiling: CHDIR... **")
 os.chdir("./payloads/")
 print("** Compiling: Invoke javac... **")
-subprocess.run(["javac", "run.java"])
+subprocess.run(["javac", "Log4Warn.java"])
 print("** DONE.. Opening webserver! **")
 subprocess.run(["python3", "-m", "http.server", "8888"])
 
